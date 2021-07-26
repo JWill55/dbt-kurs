b917b404baf3d58e3ef2eff161793bf47a551d6a
bollow.marc@guj.de <bollow.marc@guj.de>
2021-07-26 18:45:24 +0000

    Sicherung Entwicklungsstand aus der Schulung

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -6,9 +6,9 @@ orders as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
-
-    from  `dbt-tutorial.jaffle_shop.orders`
+        status,
+        _etl_loaded_at
+    from {{ source('jaffle_shop', 'orders') }}
 )
 
 select *
